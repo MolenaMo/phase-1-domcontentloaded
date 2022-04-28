588103b012415bb86672009efa9c8a0d31bb7a3e
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-04-28 01:07:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+let newMsg = "This is really cool!"
+let oldMsg = document.querySelector('p')
+document.addEventListener('DOMContentLoaded', () =>{
+    oldMsg.innerHTML = newMsg
+})
+
+//code below also works, but wouldn't pass test-requirment to use querySelector
+// let newPtag = document.getElementById("text").textContent = newMsg
